65db5fcb6cc01b1189cd3a2fda698909d3000cd0
Ian Hall <ianmhall6@gmail.com>
2022-08-17 16:53:26 -0400

    bug fix for NavBar.  Now button will not appear in screen width 500+

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -97,7 +97,7 @@ function Navbar() {
       
     }}className="btn"><b>ⓑ</b></button> */}
 
-    {(toggleMenu || screenWidth <= 500) && (
+    {(screenWidth <= 500) && (
     <button style={{
       color: 'white',
       border: 'solid 2px',
@@ -105,9 +105,6 @@ function Navbar() {
       backgroundColor: 'rgb(57, 22, 159, 0.2)',
       fontSize: '25px',
       padding: '0 10px',
-      
-      
-      
     }} onClick={toggleNav} className="btn">≡</button>
     )}
     </nav>
@@ -117,8 +114,5 @@ function Navbar() {
 
 export default Navbar;
 
-{/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
-</svg> */}
 
 
